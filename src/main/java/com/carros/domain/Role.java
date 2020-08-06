0ae11c0f00291e6bcc7be11e5532b64f7f32d717
Augusto Catanho <augustocatanho@gmail.com>
2020-08-06 19:46:24 -0300

    Ajuste no Projeto

---
+++ src/main/java/com/carros/domain/Role.java
@@ -0,0 +1,28 @@
+package com.carros.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Role implements GrantedAuthority {
+	
+	private static final long serialVersionUID = 6410012658227848125L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
+}
